14de970b82019501bba4d6bd302661ebc9502150
David Huang <david.huang@getac.com.tw>
2019-05-27 08:48:09 +0800

    Setup event listeners
    Keypress & click.

--- app.js
+++ app.js
@@ -1,21 +1,34 @@
+// BUDGET CONTROLLER
 var budgetController = (function () {
-	var x = 23;
 
-	var add = function (a) {
-		return x + a;
-	}
-
-	return {
-		publicTest: function (b) {
-			console.log(add(b));
-		}
-	}
 })();
 
+// UI CONTROLLER
 var UIController = (function () {
 
 })();
 
+// GLOBAL APP CONTROLLER
 var controller = (function (budgetCtrl, UICtrl) {
+
+	var ctrlAddItem = function () {
+		// get the filed input data
+
+		// add the item to the budget controller
+
+		// add the item to UI
+
+		//  calculate the budget
+
+		// display the budget on the UI
+	}
+
+	document.querySelector('.add__btn').addEventListener('click', ctrlAddItem);
+
+	document.addEventListener('keypress', function (e) {
+		if (e.keyCode === 13 || e.which === 13) {
+			ctrlAddItem();
+		}
+	});
 
 })(budgetController, UIController);
