5df93606f34107448f58f4f2e467ee1122dd38dd
David Huang <david.huang@getac.com.tw>
2019-05-27 09:46:06 +0800

    Read input data
    Save input value as object.

--- app.js
+++ app.js
@@ -6,14 +6,36 @@ var budgetController = (function () {
 // UI CONTROLLER
 var UIController = (function () {
 
+	var DOMstrings = {
+		inputType: '.add__type',  // +/-
+		inputDescription: '.add__description',
+		inputValue: '.add__value',
+		inputBtn: '.add__btn',
+	}
+
+	return {
+		getinput: function () {
+			return {
+				type: document.querySelector(DOMstrings.inputType).value,
+				description: document.querySelector(DOMstrings.inputDescription).value,
+				value: document.querySelector(DOMstrings.inputValue).value
+			};
+		},
+		getDOMstrings: function () {
+			return DOMstrings;
+		}
+	};
+
 })();
 
 // GLOBAL APP CONTROLLER
 var controller = (function (budgetCtrl, UICtrl) {
 
+	var DOM = UICtrl.getDOMstrings();
+
 	var ctrlAddItem = function () {
 		// get the filed input data
-
+		var input = UICtrl.getinput();
 		// add the item to the budget controller
 
 		// add the item to UI
@@ -23,12 +45,14 @@ var controller = (function (budgetCtrl, UICtrl) {
 		// display the budget on the UI
 	}
 
-	document.querySelector('.add__btn').addEventListener('click', ctrlAddItem);
+	document.querySelector(DOM.inputBtn).addEventListener('click', ctrlAddItem);
 
 	document.addEventListener('keypress', function (e) {
+
 		if (e.keyCode === 13 || e.which === 13) {
 			ctrlAddItem();
 		}
+
 	});
 
 })(budgetController, UIController);
