f3f320e9cfc98f5fd80fe35ef57365066d4c34a0
David Huang <david.huang@getac.com.tw>
2019-05-27 16:49:31 +0800

    Create html string before insertion

--- app.js
+++ app.js
@@ -67,6 +67,21 @@ var UIController = (function () {
 				value: document.querySelector(DOMstrings.inputValue).value
 			};
 		},
+		addListItem: function (obj, type) {
+			var html;
+			// create html string with placeholder text
+			if (type === 'inc') {
+				html = '<div class="item clearfix" id="income-%id%">< div class="item__description" > %description%</div ><div class="right clearfix">< div class="item__value" >%value%</div ><div class="item__delete"><button class="item__delete--btn"><i class="ion-ios-close-outline"></i></button></div></div ></div >';
+			} else if (type === 'exp') {
+				html = '<div class="item clearfix" id="income-1"><div class="item__description">%description%</div><div class="right clearfix"><div class="item__value">%value%</div><div class="item__delete"><button class="item__delete--btn"><i class="ion-ios-close-outline"></i></button></div></div></div>';
+			}
+			// replace the placeholder text with some actual data
+			newHtml = html.replace('%id%', obj.id);
+			newHtml = newHtml.replace('%description%', obj.description);
+			newHtml = newHtml.replace('%value%', obj.value);
+
+			// insert the html into the dom
+		},
 		getDOMstrings: function () {
 			return DOMstrings;
 		}
