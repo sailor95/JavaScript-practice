b31f7be88c37f95a07f8c333be5acaf06848b082
David Huang <david.huang@getac.com.tw>
2019-06-03 16:08:14 +0800

    Calculate budget and percentage

--- app.js
+++ app.js
@@ -13,6 +13,14 @@ var budgetController = (function () {
 		this.value = value;
 	}
 
+	var calculateTotal = function (type) {
+		var sum = 0;
+		data.allItems[type].forEach(function (current) {
+			sum = sum + current.value;
+		});
+		data.totals[type] = sum;
+	};
+
 	var data = {
 		allItems: {
 			exp: [],
@@ -21,7 +29,9 @@ var budgetController = (function () {
 		totals: {
 			exp: 0,
 			inc: 0
-		}
+		},
+		budget: 0,
+		percentage: -1
 	}
 
 	return {
@@ -44,6 +54,35 @@ var budgetController = (function () {
 			data.allItems[type].push(newItem);
 
 			return newItem;
+		},
+
+		calculateBudget: function () {
+			// calculate total income and expenses
+			calculateTotal('exp');
+			calculateTotal('inc');
+
+			// calculate the budget: income - expenses
+			data.budget = data.totals.inc - data.totals.exp;
+
+			// calculate the percentage of income that we spent
+			if (data.totals.inc > 0) {
+				data.percentage = Math.round((data.totals.exp / data.totals.inc) * 100);
+			} else {
+				data.percentage = -1;
+			}
+		},
+
+		getBudget: function () {
+			return {
+				budget: data.budget,
+				totalInc: data.totals.inc,
+				totalExp: data.totals.exp,
+				percentage: data.percentage
+			};
+		},
+
+		testing: function () {
+			console.log(data);
 		}
 	};
 
@@ -124,9 +163,9 @@ var controller = (function (budgetCtrl, UICtrl) {
 
 	var updateBudget = function () {
 		// calculate the budget
-
+		budgetCtrl.calculateBudget();
 		// return the budget
-
+		var budget = budgetCtrl.getBudget();
 		// display the budget on the UI
 	}
 
