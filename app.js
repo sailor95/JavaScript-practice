68881a874b1e8eddbda06a80e952688c9cb627d8
David Huang <david.huang@getac.com.tw>
2019-06-11 15:32:03 +0800

    Expense UI toggle red

--- app.js
+++ app.js
@@ -161,6 +161,12 @@ var UIController = (function () {
 		return (type === 'exp' ? '-' : '+') + ' ' + int + '.' + dec;
 	}
 
+	var nodeListForEach = function (list, callback) {
+		for (var i = 0; i < list.length; i++) {
+			callback(list[i], i);
+		}
+	};
+
 	return {
 		getinput: function () {
 			return {
@@ -224,12 +230,6 @@ var UIController = (function () {
 		displayPercentages: function (percentages) {
 			var fields = document.querySelectorAll(DOMstrings.expensesPercLabel);
 
-			var nodeListForEach = function (list, callback) {
-				for (var i = 0; i < list.length; i++) {
-					callback(list[i], i);
-				}
-			}
-
 			nodeListForEach(fields, function (current, index) {
 				if (percentages[index] > 0) {
 					current.textContent = percentages[index] + '%';
@@ -247,6 +247,20 @@ var UIController = (function () {
 			month = now.getMonth();
 			year = now.getFullYear();
 			document.querySelector(DOMstrings.dateLabel).textContent = months[month] + ' ' + year;
+		},
+
+		changedType: function () {
+			var fields = document.querySelectorAll(
+				DOMstrings.inputType + ',' +
+				DOMstrings.inputDescription + ',' +
+				DOMstrings.inputValue
+			);
+
+			nodeListForEach(fields, function (cur) {
+				cur.classList.toggle('red-focus');
+			});
+
+			document.querySelector(DOMstrings.inputBtn).classList.toggle('red');
 		},
 
 		getDOMstrings: function () {
@@ -271,6 +285,7 @@ var controller = (function (budgetCtrl, UICtrl) {
 		});
 
 		document.querySelector(DOM.container).addEventListener('click', ctrlDeleteItem);
+		document.querySelector(DOM.inputType).addEventListener('change', UICtrl.changedType);
 	};
 
 	var updateBudget = function () {
